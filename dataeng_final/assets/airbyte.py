184b0c1dfe9e1f1980a0e6fce7bd9d3e5f375542
Pedro Araujo <dodobeatle@gmail.com>
2025-04-18 01:38:26 -0300

    Small changes

--- dataeng_final/assets/airbyte.py
+++ dataeng_final/assets/airbyte.py
@@ -4,5 +4,5 @@ from dataeng_final.resources import airbyte_resource
 #airbyte_assets = load_assets_from_airbyte_instance(airbyte_resource)
 
 airbyte_connections = load_assets_from_airbyte_instance(airbyte_resource, 
-                                                        key_prefix="recommmender_system_raw", 
+                                                        key_prefix="mlops_raw", 
                                                         connection_to_group_fn=lambda connection_name: "raw_data_ingestion")
