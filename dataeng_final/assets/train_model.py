442e21a091971e1b59b099cb7b88515442095526
Pedro Araujo <dodobeatle@gmail.com>
2025-04-19 02:06:46 -0300

    Some progress in the ML model

--- dataeng_final/assets/train_model.py
+++ dataeng_final/assets/train_model.py
@@ -1,21 +1,82 @@
-from dagster import asset, AssetIn, AssetKey, Output
+from dagster import asset, AssetIn, AssetKey, Output, multi_asset, AssetOut, AssetExecutionContext
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+
+@multi_asset(
+    group_name="data_preparation",
+    ins={"preprocessed_data": AssetIn(key=AssetKey("movies"),
+        input_manager_key= "postgres_io_manager")
+    },
+    outs={
+        "preprocessed_training_data": AssetOut(),
+        "user2Idx": AssetOut(),
+        "movie2Idx": AssetOut() 
+    }
+)
+def preprocessed_data(context, preprocessed_data: pd.DataFrame):
+    """
+    Preprocesses the data for training the model.
+    """
+   # rows_count = len(preprocessed_data)
+
+    user2Idx = {}
+    movie2Idx = {}
+    preprocessed_training_data = preprocessed_data.copy()
+    
+    return preprocessed_training_data, user2Idx, movie2Idx 
+
+@multi_asset(
+    group_name="data_preparation",
+    ins={ "preprocessed_training_data": AssetIn()
+    },
+    outs={
+        "X_train": AssetOut(),
+        "X_test": AssetOut(),
+        "y_train": AssetOut(),
+        "y_test": AssetOut()
+    }
+)
+def split_data(context: AssetExecutionContext, preprocessed_training_data):
+    """
+    Splits the data for training the model.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(preprocessed_training_data, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
 
 
 
 @asset(
-        ins={"preprocessed_data": AssetIn(key=AssetKey("movies"),
-             input_manager_key= "postgres_io_manager")}
+    group_name="model_training",
+    ins={
+        "X_train": AssetIn(),
+        "y_train": AssetIn(),
+        "user2Idx": AssetIn(),
+        "movie2Idx": AssetIn(),
+    }
 )
-def preprocessed_data(context, preprocessed_data: pd.DataFrame)->Output[pd.DataFrame]:
-    """
-    Preprocesses the data for training the model.
-    """
-    rows_count = len(preprocessed_data)
+def model_trained(context, X_train, y_train, user2Idx, movie2Idx):
+    model = {}
+    return model
 
-    return Output(
-        preprocessed_data,
-        metadata={
-            "rows_count": rows_count
-        }
-    )
+@asset(
+    group_name="model_training",
+    ins={
+        "model_trained": AssetIn(),
+    }
+)
+def model_stored(context, model_trained):
+    return ...
+
+
+@asset(
+    group_name="model_evaluation",
+    ins={
+        "model_stored": AssetIn(),
+        "X_test": AssetIn(),
+        "y_test": AssetIn(),
+    }
+)
+def model_metrics(context, model_stored, X_test, y_test):
+    return ...  
