341ccfb61f47f73d2add158285c1a463af0b4506
Pedro Araujo <dodobeatle@gmail.com>
2025-04-25 17:53:14 -0300

    hasta solucion de cursor

--- dataeng_final/assets/train_model.py
+++ dataeng_final/assets/train_model.py
@@ -1,4 +1,4 @@
-from dagster import asset, AssetIn, AssetKey, Output, multi_asset, AssetOut, AssetExecutionContext
+from dagster import asset, AssetIn, AssetKey, Output, multi_asset, AssetOut, AssetExecutionContext, Config
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -50,13 +50,25 @@ def split_data(context: AssetExecutionContext, preprocessed_training_data):
         'test_size': 0.4,
         'random_state': 42
     }
+
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, **split_param)
+
+    print(X_train.columns)
+
     return X_train, X_test, y_train, y_test
 
 
+class MyModelConfig(Config):
+    batch_size : int = 128
+    epochs : int = 10
+    learning_rate: float = 1e-3
+    embeddings_dim: int = 5
+    
 
 @asset(
+    required_resource_keys={"mlflow"},
+
     group_name="model_training",
     ins={
         "X_train": AssetIn(),
@@ -65,21 +77,74 @@ def split_data(context: AssetExecutionContext, preprocessed_training_data):
         "movie2Idx": AssetIn(),
     }
 )
-def model_trained(context, X_train, y_train, user2Idx, movie2Idx):
-    model = {}
+def model_trained(context, config:MyModelConfig, X_train, y_train, user2Idx, movie2Idx):
+    from .model_helper import get_model
+    from keras.optimizers import Adam
+    
+
+    mlflow = context.resources.mlflow
+    mlflow.log_params(context.op_config)
+    mlflow.tensorflow.autolog()
+    
+    batch_size = config.batch_size
+    epochs = config.epochs
+    learning_rate = config.learning_rate
+    embeddings_dim = config.embeddings_dim
+
+    model = get_model(len(movie2Idx), len(user2Idx), embeddings_dim)
+
+    model.compile(Adam(learning_rate=learning_rate), 'mean_squared_error')
+    
+    context.log.info(f'batch_size: {batch_size} - epochs: {epochs}')
+    
+    history = model.fit(
+        [
+            X_train.user_id,
+            X_train.movie_id
+        ], 
+        y_train, 
+        batch_size=batch_size,
+        # validation_data=([ratings_val.userId, ratings_val.movieId], ratings_val.rating), 
+        epochs=epochs, 
+        verbose=1
+    )
+    for i, l in enumerate(history.history['loss']):
+        mlflow.log_metric('mse', l, i)
+    from matplotlib import pyplot as plt
+    fig, axs = plt.subplots(1)
+    axs.plot(history.history['loss'], label='mse')
+    plt.legend()
+    mlflow.log_figure(fig, 'plots/loss.png')
     return model
 
 @asset(
+    required_resource_keys={"mlflow"},
+
     group_name="model_training",
     ins={
         "model_trained": AssetIn(),
     }
 )
 def model_stored(context, model_trained):
-    return ...
-
+    import numpy as np
+    mlflow = context.resources.mlflow
+    
+    logged_model = mlflow.tensorflow.log_model(
+        model_trained,
+        "keras_dot_product_model",
+        registered_model_name='keras_dot_product_model',
+        # input_example=[np.array([1, 2]), np.array([2, 3])],
+    )
+    # logged_model.flavors
+    model_data = {
+        'model_uri': logged_model.model_uri,
+        'run_id': logged_model.run_id
+    }
+    return model_data
 
 @asset(
+    required_resource_keys={"mlflow"},
+
     group_name="model_evaluation",
     ins={
         "model_stored": AssetIn(),
@@ -88,4 +153,21 @@ def model_stored(context, model_trained):
     }
 )
 def model_metrics(context, model_stored, X_test, y_test):
-    return ...  
+    mlflow = context.resources.mlflow
+    logged_model = model_stored['model_uri']
+
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+    
+    y_pred = loaded_model.predict([
+            X_test.encoded_user_id,
+            X_test.encoded_movie_id
+    ])
+    from sklearn.metrics import mean_squared_error
+
+    mse = mean_squared_error(y_pred.reshape(-1), y_test.rating.values)
+    metrics = {
+        'test_mse': mse,
+        'test_rmse': mse**(0.5)
+    }
+    mlflow.log_metrics(metrics)
+    return metrics  
--- dataeng_final/definitions.py
+++ dataeng_final/definitions.py
@@ -1,9 +1,19 @@
-from dagster import Definitions, load_assets_from_modules, define_asset_job, AssetSelection, ScheduleDefinition
+from pathlib import Path
+import os
+import yaml
+from dagster import Definitions, load_assets_from_modules, define_asset_job, AssetSelection, ScheduleDefinition, file_relative_path
 from dataeng_final.assets.airbyte import airbyte_connections
 from dataeng_final import assets  # noqa: TID252
-
+from dagster_mlflow import mlflow_tracking
 from dataeng_final.assets import dbt, train_model   
 from dataeng_final.resources import dbt_resource, postgres_io_manager
+
+
+
+# Load the cofigs.yml file (config parameters file)
+config_file_path = file_relative_path(__file__, "configs.yml")
+with open(config_file_path, "r") as file:
+    config_parameters = yaml.safe_load(file)
 
 #--------------------------------Assets Definitions--------------------------------    
 
@@ -17,6 +27,10 @@ airbyte_sync_job = define_asset_job(name="airbyte_sync_job",  selection=AssetSel
 dbt_sync_job = define_asset_job("dbt_sync_job" , selection=AssetSelection.groups("raw_data_transformation"))
 
 data_prep_job = define_asset_job("data_prep_job", selection=AssetSelection.groups("data_preparation"))
+
+model_training_job = define_asset_job("model_training_job", selection=AssetSelection.groups("model_training"))
+
+model_evaluation_job = define_asset_job("model_evaluation_job", selection=AssetSelection.groups("model_evaluation"))
 
 sync_all_jobs = define_asset_job(name="sync_all_jobs", selection=AssetSelection.all())
 
@@ -32,12 +46,14 @@ daily_sync_schedule = ScheduleDefinition(
 #--------------------------------Definitions--------------------------------    
 defs = Definitions(
     assets=[airbyte_connections, *dbt_assets, *train_assets],
-    jobs=[airbyte_sync_job, dbt_sync_job, data_prep_job, sync_all_jobs],
+    jobs=[airbyte_sync_job, dbt_sync_job, data_prep_job, model_training_job, model_evaluation_job, sync_all_jobs],
     resources={
         "dbt": dbt_resource, 
         "postgres_io_manager": postgres_io_manager.configured({
             "connection_string": "env:POSTGRES_CONNECTION_STRING",
             "schema": "target"}),
+        "mlflow": mlflow_tracking.configured(config_parameters['mlflow']),
+
     },
     schedules=[daily_sync_schedule]
 )
